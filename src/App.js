75d9e64bfa4c5e0536fa3846a4f26cb357b853b7
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-04 19:42:54 -0400

    Restructure app to have carousel-like pages Add swipe functionality for screens Add fadeIn and fadeOut effects for pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import React, { useState } from 'react';
+import Reveal from 'react-reveal/Reveal'
+import withReveal from 'react-reveal/withReveal'
 import Home from './Home';
 import About from './About'
 import Contact from './Contact'
 import Projects from './Projects'
 
+const pages = [Home, About, Projects, Contact]
+const pagesWithReveal = pages.map(Page => withReveal(Page, <Reveal effect='animate__fadeIn' effectOut="animate__fadeOut" />))
+
 function App() {
+  const [ page, setPage ] = useState(0)
+  const [ lastX, setLastX ] = useState(0)
+
+  const handlePageUp = () => {
+    if (page < pages.length - 1) {
+      setPage(page + 1)
+    }
+  }
+
+  const handlePageDown = () => {
+    if (page > 0) {
+      setPage(page - 1)
+    }
+  }
+
+  const handleStartTouch = e => {
+    setLastX(e.touches[0].clientX)
+  }
+
+  const handleSwipe = e => {
+    const validSwipeLength = 50
+    const currentX = e.changedTouches[0].clientX
+    if ( (currentX < lastX) && (lastX - currentX > validSwipeLength) ) {
+      handlePageUp()
+    } else if ( (currentX > lastX) && (currentX - lastX > validSwipeLength) ) {
+      handlePageDown()
+    }
+  }
+
+  const CurrentPage = pagesWithReveal[page]
   return (
-    <div className="main">
-      <div className="container py-3 py-lg-5 mh-100">
-        <div className="row justify-content-center align-items-center">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/projects" component={Projects} />
-          </Switch>
+    <div className="main mx-3 mx-lg-5 mb-3">
+      <div className="d-flex flex-row justify-content-center align-items-center">
+        <div onClick={handlePageDown} className="bar">
+          {'<'}
+        </div>
+        <div onTouchStart={handleStartTouch} onTouchEnd={handleSwipe} className="d-flex flex-column justify-content-center align-items-stretch">
+          <CurrentPage /> 
+        </div>
+        <div onClick={handlePageUp} className="bar">
+          {'>'}
         </div>
       </div>
     </div>
