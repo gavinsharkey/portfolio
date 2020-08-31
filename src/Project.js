64a37e5a5c743493aa6d3065a6c24708f71031a1
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-31 10:16:31 -0400

    Add description Change outer div border radius

--- src/Project.js
+++ src/Project.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-export default function Project({ title, subtitle, github, liveDemo, videoDemo }) {
+export default function Project({ title, subtitle, description, github, liveDemo, videoDemo }) {
   return (
-    <div className="card">
+    <div style={{borderRadius: '0.5em'}} className="card mt-3">
         <div className="card-body">
           <h4 className="card-title">{title}</h4>
-          <h6 className="card-subtitle text-muted">{subtitle}</h6>
+          <h6 className={`${description ? 'border-bottom' : null} card-subtitle text-muted`}>{subtitle}</h6>
+          <div className="mt-1">
+            <p className="card-text">{description}</p>
+          </div>
           <div className="my-3 border-bottom">
             <h5 className="m-0">Links:</h5>
           </div>
