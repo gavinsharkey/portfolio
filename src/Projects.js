581bcce8fdce0eb255c86b98f580113962211bbc
gavinsharkey <gavinsharkey2016@gmail.com>
2020-08-31 10:16:45 -0400

    Add Nibbl project

--- src/Projects.js
+++ src/Projects.js
@@ -4,13 +4,21 @@ import Project from './Project'
 export default function Projects() {
   return (
     <div className='p-4 main-block d-flex flex-column'>
-      <div className="border-bottom mb-3">
+      <div className="border-bottom">
         <h2>Projects</h2>
       </div>
       <Project
         title="Portfolio Site"
         subtitle="You're looking at it!"
         github="https://github.com/gavinsharkey/portfolio"
+      />
+      <Project
+        title="Nibbl"
+        subtitle="A Concise Social Media"
+        description="This is Nibbl. Basically, this is a social media with one key quirk: 30-character limit. You have to be very concise. I took a lot of inspiration from Twitter, so in many ways it's a simplifyed version of that. I hope you enjoy!"
+        github="https://github.com/gavinsharkey/nibbl_app_react_project"
+        liveDemo="https://gavinsharkey.github.io/nibbl"
+        videoDemo="https://youtu.be/_9M5TPCa2_s"
       />
       <Project
         title='Disney Trip Planner'
