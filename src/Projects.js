e1d8b6bdf71e168dcad0bb0692869fc24532b0e6
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-13 16:16:17 -0400

    fetch projects from database

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Project from './Project'
 
 export default function Projects() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    fetch('https://gavin-sharkey-projects-api.herokuapp.com/projects')
+    .then(resp => resp.json())
+    .then(json => setProjects(json))
+  }, [])
+
   return (
     <div className='p-4 main-block d-flex flex-column'>
       <div className="border-bottom">
@@ -12,21 +20,16 @@ export default function Projects() {
         subtitle="You're looking at it!"
         github="https://github.com/gavinsharkey/portfolio"
       />
-      <Project
-        title="Nibbl"
-        subtitle="A Concise Social Media"
-        description="This is Nibbl. Basically, this is a social media with one key quirk: 30-character limit. You have to be very concise. I took a lot of inspiration from Twitter, so in many ways it's a simplifyed version of that. I hope you enjoy!"
-        github="https://github.com/gavinsharkey/nibbl_app_react_project"
-        liveDemo="https://gavinsharkey.github.io/nibbl"
-        videoDemo="https://youtu.be/_9M5TPCa2_s"
-      />
-      <Project
-        title='Disney Trip Planner'
-        subtitle="A JavaScript/HTML app for planning a trip to Walt Disney World"
-        github="https://github.com/gavinsharkey/disney-trip-js-project"
-        liveDemo="https://gavinsharkey.github.io/disney-trip-planner-client/"
-        videoDemo="https://youtu.be/HgX-bZ6ZuRM"
-      />
+      { projects.map(project => (
+        <Project
+          title={project.title}
+          subtitle={project.subtitle}
+          description={project.description}
+          github={project.github}
+          liveDemo={project.demo}
+          videoDemo={project.video}
+        />
+      )) }
     </div>
   )
 }
