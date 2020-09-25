b8e93ad277624f79fa5f0be12ee69ce47e3a0686
gavinsharkey <gavinsharkey2016@gmail.com>
2020-09-25 10:47:43 -0400

    add loading indicator to projects

--- src/Projects.js
+++ src/Projects.js
@@ -2,13 +2,30 @@ import React, {useState, useEffect} from 'react'
 import Project from './Project'
 
 export default function Projects() {
+  const [isLoading, setIsLoading] = useState(true)
   const [projects, setProjects] = useState([])
 
   useEffect(() => {
     fetch('https://gavin-sharkey-projects-api.herokuapp.com/projects')
     .then(resp => resp.json())
-    .then(json => setProjects(json))
+    .then(json => {
+      setProjects(json)
+      setIsLoading(false)
+    })
   }, [])
+
+  const projectsList = () => {
+    return projects.map(project => (
+      <Project
+        title={project.title}
+        subtitle={project.subtitle}
+        description={project.description}
+        github={project.github}
+        liveDemo={project.demo}
+        videoDemo={project.video}
+      />
+    ))
+  }
 
   return (
     <div className='p-4 main-block d-flex flex-column'>
@@ -20,16 +37,9 @@ export default function Projects() {
         subtitle="You're looking at it!"
         github="https://github.com/gavinsharkey/portfolio"
       />
-      { projects.map(project => (
-        <Project
-          title={project.title}
-          subtitle={project.subtitle}
-          description={project.description}
-          github={project.github}
-          liveDemo={project.demo}
-          videoDemo={project.video}
-        />
-      )) }
+      { isLoading
+      ? <h4 className="text-center mt-3">Loading...</h4>
+      : projectsList() }
     </div>
   )
 }
