0c7e19a2db2f14b06e8adfae0f8a5638ddaffdad
gavinsharkey <gavinsharkey2016@gmail.com>
2020-11-02 13:37:33 -0500

    Add new image to About page

--- src/About.js
+++ src/About.js
@@ -2,15 +2,22 @@ import React from 'react'
 
 export default function About() {
   return (
-      <div className="p-4 main-block d-flex flex-column flex-lg-row-reverse align-items-center align-items-lg-start">
+      <div className="p-4 main-block d-flex flex-column flex-lg-row-reverse align-items-center">
         <div className="field">
-          <img className="img-fluid picture" width="300px" src='/portfolio/IMG_6606.JPG' alt="Gavin Sharkey" />
+          <img className="img-fluid picture" width="300px" src='/portfolio/second_image.JPG' alt="Gavin Sharkey" />
         </div>
-        <div className="field mt-2 mt-lg-0 col-lg-10">
+        <div className="field mt-2 mt-lg-0 col-lg-9">
           <div className="border-bottom">
             <h2>About</h2>
           </div>
-          <p>I am a full stack web developer with proficiency in React, Rails, Javascript, and Ruby. As a homeschooled student, a passion for learning was instilled in me at a very young age. Since my early teens, I sought out learning such skills as Algebra, through which I developed a love for problem-solving, and Photoshop, which channeled my desire for creative expression. My appreciation for the logic of Algebra, mixed with my desire to create, has led me to pursue a career in software development, to which I bring my capacity and passion to learn. </p>        
+          <p>Full stack web developer with proficiency in React, Rails, JavaScript, and Ruby. I've always had a passion for the problem-solving, logical nature of Algebra and Mathematics in general. Now I channel that passion towards solving problems across the stack, making awesome software.
+            <br />
+            <br />
+            My other main priority in life is fitness. Prior to Flatiron, I used to be a running specialist at Fit2Run. My favorite responsibility was getting to pace half-marathons. It filled me with joy knowing that my leadership allowed other to set new PRs, or even just complete their first half-marathon! And now I carry that passion for leadership and supporting my team into everything I do.
+            <br />
+            <br />
+            My main focus now is growing as a developer, and finding a place where I can work with an equally passionate team, to make something great!
+          </p>        
         </div>
       </div>
   )
